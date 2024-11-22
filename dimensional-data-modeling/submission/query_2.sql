5b80be2726cf122641cf4e688b294ce7a3242f4e
Dion Okafor <49908077+okaforoa@users.noreply.github.com>
2024-11-22 16:36:54 -0500

    Update query_2.sql

---
+++ dimensional-data-modeling/submission/query_2.sql
@@ -0,0 +1,17 @@
+INSERT INTO bootcamp.actors
+SELECT
+  actor,
+  actor_id,
+  ARRAY_AGG(
+    ROW(film, votes, rating, film_id)
+  ) AS films,
+  CASE
+    WHEN AVG(rating) > 8 THEN 'star'
+    WHEN AVG(rating) > 7 THEN 'good'
+    WHEN AVG(rating) > 6 THEN 'average'
+    ELSE 'bad'
+  END AS quality_class,
+  MAX(current_year) = EXTRACT(YEAR FROM CURRENT_DATE) AS is_active,
+  current_year
+FROM actor_films
+GROUP BY actor, actor_id, current_year;
